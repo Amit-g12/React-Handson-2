5367cb5c9982256649bc8bfb60fa04175de3cb38
Amit Kumar <amitkumargang123@gmail.com>
2022-09-01 18:38:52 +0530

    react handson-2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react'
+import AForm from './Components/AForm';
+
+class Form extends React.Component{
+
+ render(){
+ return(
+<div>
+ <AForm/>
+</div>
+ )
+}
+}
+export default Form
